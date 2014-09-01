ba54378910b95b5d77cf0bb3d4d503d9e41c7310
Mai Phan <mai.phan@eastagile.com>
2014-09-01 18:01:03 +0700

    secret token

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,16 @@ class HomeController < ActionController::Base
   end
 
   def payload
+    request.body.rewind
+    payload_body = request.body.read
+    verify_signature(payload_body)
+    push = JSON.parse(params[:payload])
+    puts "I got some JSON: #{push.inspect}"
     system "bundle exec coleslaw --token DjpBxEx_dL3Z5VK8Y2py xruywz4pyMSTzmJfUEzz"
+  end
+
+  def verify_signature(payload_body)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 end
