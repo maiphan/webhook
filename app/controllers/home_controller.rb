6b99dd28616ed042a2c50d22e5ba54b58bd10c63
Mai Phan <mai.phan@eastagile.com>
2014-09-01 18:04:07 +0700

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ActionController::Base
   end
 
   def payload
-    debugger
     request.body.rewind
     payload_body = request.body.read
+    debugger
     verify_signature(payload_body)
     push = JSON.parse(params[:payload])
     puts "I got some JSON: #{push.inspect}"
@@ -16,6 +16,7 @@ class HomeController < ActionController::Base
   end
 
   def verify_signature(payload_body)
+    debugger
     signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
     return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
