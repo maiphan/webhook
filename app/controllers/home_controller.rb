73fb10fe21f92245d133539e7c45e94da08d1d96
Mai Phan <mai.phan@eastagile.com>
2014-09-01 17:17:26 +0700

    hei

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
 
   end
 
   def payload
-    exec "bundle exec coleslaw --token DjpBxEx_dL3Z5VK8Y2py xruywz4pyMSTzmJfUEzz"
-    redirect_to :back
+    system "bundle exec coleslaw --token DjpBxEx_dL3Z5VK8Y2py xruywz4pyMSTzmJfUEzz"
   end
 end
